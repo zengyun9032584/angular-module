81175dde3f5bd83f04a097cb633632fe69dd7946
zengyun <zengyunzengyun@outlook.com>
2017-10-31 16:04:26 +0800

    添加ngFor的过滤管道

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+/**
+ *
+ * 提供ngFor的关键字过滤功能
+ * @export
+ * @class FilterPipe
+ * @implements {PipeTransform}
+ */
+@Pipe({
+  name: 'filter',
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: any, ...args): any {
+    try {
+      return value.filter(item => {
+        // 返回过滤后的内容,需在使用时指定过滤的属性名
+        return (item[args[1]].indexOf(args[0]) >= 0);
+      });
+    } catch (error) {
+      return value;
+    }
+  }
+
+}
