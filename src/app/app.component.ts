58d1df6c28c074b63206a194e0b2fe5e278a3763
zengyun <375836301@qq.com>
2018-01-21 20:03:11 +0800

    更新注释信息
    Signed-off-by: zengyun <375836301@qq.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+/**
+ *
+ * 系统根组件
+ * @export
+ * @class AppComponent
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,11 +17,12 @@ export class AppComponent {
   public smallScreen = false;    // 判断是否是小屏幕
   private linkTotal = 5;    // 记录<a>标签路由总个数
   private startLocX: number;    // 保存触控事件触发时的X坐标
-  private routeArray = [];
+  private routeArray = [];    // 存放每个模块的路由地址
 
   constructor(private route: Router) {
     // 初始化路由列表
     this.routeArray = ['scroll-load', 'filter', 'div-bind', 'async-service', 'time'];
+    // 判断屏幕大小
     if (document.documentElement.clientWidth >= 500) {
       this.smallScreen = false;
     } else {
@@ -26,7 +33,7 @@ export class AppComponent {
   /**
    *
    * 监听触控开始事件，获取当前触控位置
-   * @param {*} event
+   * @param {*} event 触控对象
    * @memberof AppComponent
    */
   touchStart(event: any) {
@@ -35,8 +42,8 @@ export class AppComponent {
 
   /**
    *
-   * 监听触控开始事件，判断滑动方向
-   * @param {*} event
+   * 监听触控结束事件，判断滑动方向
+   * @param {*} event 触控对象
    * @memberof AppComponent
    */
   touchEnd(event: any) {
@@ -54,6 +61,11 @@ export class AppComponent {
     }
   }
 
+  /**
+   *
+   * 使用navigate触发路由跳转
+   * @memberof AppComponent
+   */
   linkRoute() {
     this.route.navigate([`/${this.routeArray[this.selectRow - 1]}`]);
   }
